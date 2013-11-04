3ca11ccbf44f7284c019051b6fe42d8f4988902b
Jan Hovancik <conta.srdr@gmail.com>
2013-11-05 01:58:23 +0900

    added flattr button, when click on <3, need to work on show/hide

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
 	//move first article down
 	$("article").css('margin-top', ($('header').height())+15);
+        //flattr
+        $("#flattr").click(function(){ 
+          $('footer').first().append('<a href="https://flattr.com/submit/auto?user_id=conta&url=http%3A%2F%2Fgetles.tk" target="_blank"><img src="https://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0"></a>');
+        });
 });
 
 
