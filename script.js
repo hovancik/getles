529056cf811d9ca8d6a27b1ca87f471866bacc62
Jan Hovancik <conta.srdr@gmail.com>
2013-11-05 23:54:10 +0900

    footer

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ $( document ).ready(function() {
 	//move first article down
 	$("article").css('margin-top', ($('header').height())+15);
         //flattr
-        $("#flattr").click(function(){ 
-          $('footer').first().append('<a href="https://flattr.com/submit/auto?user_id=conta&url=http%3A%2F%2Fgetles.tk" target="_blank"><img src="https://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0"></a>');
+        $("#hearts").click(function(){
+          $("#flattr").show("slow");
+          $("#flattr").focus();
         });
 });
 
